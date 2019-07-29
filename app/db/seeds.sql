24c1aefe5ca25547ce72bead2b4c3e19fbd30410
Calvin <calvinctvodesign@gmail.com>
2019-07-28 22:52:39 -0700

    set up server, doesn't display the list of burger yet

---
+++ app/db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name, devoured) VALUES ('Maui Burger', true);
+INSERT INTO burgers (name, devoured) VALUES ('Kilauea', true);
+INSERT INTO burgers (name, devoured) VALUES ('Hula', true);
+INSERT INTO burgers (name, devoured) VALUES ('Big Wave', true);
+INSERT INTO burgers (name, devoured) VALUES ('Longboarder', true);
+
