5d197ce808726df97d5b3e697999e94f55ce7c5f
Calvin <calvinctvodesign@gmail.com>
2019-07-29 17:01:11 -0700

    Finished application, still need CSS

--- models/burger.js
+++ models/burger.js
@@ -8,20 +8,13 @@ var burger = {
         });
     },
     // The variables cols and vals are arrays.
-    create: function (cols, vals, cb) {
-        orm.create("burgers", cols, vals, function (res) {
+    create: function (name,cb) {
+        orm.create("burgers",name, function (res) {
             cb(res);
         });
     },
-    update: function (objColVals, condition, cb) {
-        orm.update("burgers", objColVals, condition, function (res) {
-            cb(res);
-        });
-    },
-    delete: function (id, cb) {
-        orm.delete("burgers", id, function (res) {
-            cb(res);
-        });
+    update: function (id, cb) {
+        orm.update("burgers", id, cb);
     }
 };
 
